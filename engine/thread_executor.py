5c88704e4a84912e74a0f269f778fff1132c47a3
Prakash Tiwari <38014253+prakashiit@users.noreply.github.com>
2022-03-04 20:12:07 +0530

    remove future

--- engine/thread_executor.py
+++ engine/thread_executor.py
@@ -10,13 +10,11 @@ from .testreport_generator import generate_report
 
 
 def thread_executor(test_ymls, initial_variable_dictionary, test_id, email):
-    futures = []
-
     with ThreadPoolExecutor(max_workers=3) as pool_executor:
         for test_yml in test_ymls:
             try:
                 test_executor = TestExecutor(1, test_id, initial_variable_dictionary, test_yml, None)
-                futures.append(pool_executor.submit(execute_test, test_executor, email))
+                pool_executor.submit(execute_test, test_executor, email)
                 logger.info("Task submitted")
             except Exception as e:
                 logger.error("Exception occurred", e)
